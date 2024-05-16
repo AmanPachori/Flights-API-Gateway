8c209e63723f6848dffc8e2fe0639b6233f2552c
AmanPachori <amangirrajpachori2002@gmail.com>
2024-05-16 12:19:39 +0530

    Added middlewares to authentication

--- src/utils/common/auth.js
+++ src/utils/common/auth.js
@@ -10,7 +10,6 @@ function checkPassword(plainPassword, encryptedPassword) {
     throw error;
   }
 }
-
 function createToken(input) {
   try {
     return jwt.sign(input, ServerConfig.JWT_SECRET, {
@@ -22,7 +21,17 @@ function createToken(input) {
   }
 }
 
+function verifyToken(token) {
+  try {
+    return jwt.verify(token, ServerConfig.JWT_SECRET);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
 module.exports = {
   checkPassword,
   createToken,
+  verifyToken,
 };
